59bb6eb1c25be5c647e22327ccafeebf56401195
Taylor Williams <taylor@thewilliams.net>
2018-12-10 01:04:24 +0000

    Automatically backed up by Learn

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,3 +1,22 @@
 class Song < ActiveRecord::Base
+  belongs_to :artist
+  belongs_to :genre
+  has_many :notes
   # add associations here
+
+  def artist=(song_artist_name)
+    self.artist = Artist.find_or_create_by(name: song_artist_name)
+  end
+
+  def genre_id=(song_genre_id)
+    self.genre = Genre.find(song_genre_id)
+  end
+
+  def note_contents=(song_notes)
+    song_notes.each do |note|
+      if note != ""
+        self.notes << note
+      end
+    end
+  end
 end
